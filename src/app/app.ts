076b72d535976031935a5a79d23070b492b3c694
gituser2341 <b4707099@gmail.com>
2026-02-05 06:05:29 +0530

    Initial angular frontend setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,          // 🔴 IMPORTANT
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
