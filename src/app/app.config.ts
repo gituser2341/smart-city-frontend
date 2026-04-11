61fcebed318cdfc2301d0474a32b9d5b793ff74a
gituser2341 <b4707099@gmail.com>
2026-04-11 16:39:43 +0530

    pwa error solve

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, HttpClient } from '@angular/common/http';
-import { provideServiceWorker,SwUpdate  } from '@angular/service-worker';
+import { provideServiceWorker, SwUpdate } from '@angular/service-worker';
 import { isDevMode } from '@angular/core';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 import { routes } from './app.routes';
 
 export class CustomTranslateLoader implements TranslateLoader {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
   getTranslation(lang: string): Observable<any> {
     return this.http.get(`/assets/i18n/${lang}.json`);
   }
@@ -26,15 +26,15 @@ export const appConfig: ApplicationConfig = {
       TranslateModule.forRoot({
         fallbackLang: 'en',          // ← fixed deprecation
         loader: {
-          provide:    TranslateLoader,
+          provide: TranslateLoader,
           useFactory: loaderFactory,  // ← back to useFactory
-          deps:       [HttpClient]    // ← explicitly inject HttpClient
+          deps: [HttpClient]    // ← explicitly inject HttpClient
         }
       })
-    ),  
+    ),
     provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
+      enabled: true,                          // ← change this
+      registrationStrategy: 'registerImmediately'  // ← change this
     })
   ]
 };
